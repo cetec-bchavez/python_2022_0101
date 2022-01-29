bc0130a0342f727d9329f2723d475f9f9401ac08
cetec-bchavez <cetec.bchavez@gmail.com>
2022-01-29 11:36:37 -0500

    Variables

---
+++ instructor/02_variables/02_textos.py
@@ -0,0 +1,54 @@
+nombre_mayus = ''
+nombre = 'Centro de Capacitacion'
+
+nombre_mayus = nombre.upper()
+
+print(nombre_mayus)
+
+
+print('--------------------- ANONIMAMENTE -------------------------')
+
+nombre = 'Centro de Capacitacion'
+
+print('Centro de Capacitacion'.upper())
+print(nombre.upper())
+
+print('--------------------- OTROS -------------------------')
+
+print(nombre.replace('Centro','Universidad'))
+#nombre = nombre.replace('Centro','Universidad')
+print(nombre)
+
+nombre2='Maria,Ana,Veronica,Luizana,Luis'
+
+palabras = nombre.split(' ')
+palabras_nombres = nombre2.split(',')
+
+print(palabras)
+print(palabras_nombres)
+
+for palabra in palabras_nombres :
+    print('Mujer Actual = ',palabra)    
+
+
+print('--------------------- Ejercicio Pregunta -------------------------')
+
+for palabra in palabras_nombres :
+    print('Mujer Actual = ',palabra)
+
+print('--------------------- Ejercicio Nombre Completo -------------------------')
+
+nombres3='Maria Perez,Ana Sanchez,Veronica Gutierrez,Luizana Perez,Luis Perez'
+palabras_nombres3 = nombres3.split(',')
+
+for palabra in palabras_nombres3 :
+    print('Mujer Completo = ',palabra)
+    print('Mujer Nombre Simple = ',palabra.split(' ')[0])
+    print('Mujer Apellido Simple = ',palabra.split(' ')[1])
+    """
+    nombre_simple = palabra.split(' ')[0]
+    apellid_simple = palabra.split(' ')[1]
+
+    print('Mujer Nombre Simple = ',nombre_simple)
+    print('Mujer Apellido Simple = ',apellid_simple)
+    """
