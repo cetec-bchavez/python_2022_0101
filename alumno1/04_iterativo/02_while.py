7fb69f3f2116e00b14281c0e589cca55af871c3c
ROMARDEL <richardomardelgado@hotmail.com>
2022-01-29 12:49:02 -0500

    :)

---
+++ alumno1/04_iterativo/02_while.py
@@ -0,0 +1,28 @@
+print('------WHILE ECUASION------')
+
+i=5
+y=0
+
+# while pregunta <= 50:
+i = 5
+while i <= 50:
+    y = 5*i + 30   
+    print(f'i{i}, y={y}')
+
+    i += 5
+
+print('------ WHILE ECUASION ------')
+
+i=5
+
+i = 5
+while i <= 50:
+
+    if i==30:
+        i+=5   
+        continue
+
+    y = 5*i + 30   
+    print(f'i{i}, y={y}')
+
+    i += 5
