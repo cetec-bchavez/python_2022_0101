3a587f95e86604ee141f6d3595491fed3a2db40d
ROMARDEL <richardomardelgado@hotmail.com>
2022-01-29 20:24:43 -0500

    :)
    :)

--- alumno1/02_variables/02_textos.py
+++ alumno1/02_variables/02_textos.py
@@ -1,32 +1,22 @@
 from dataclasses import replace
 
-
 nombre = 'cec'
-
-
 nombre_mayus = nombre.upper()
-
 print(nombre_mayus)
 
 
 print('--------------ANONIMAMENTE----------------')
-
 print('cec'.upper())
 
-
 print('-----OTROS-----')
-
 nombre = 'cec'
-
 print(nombre.replace('cec', 'cetec'))
 #nombre = nombre.replace('centro', 'universidad')
 print(nombre)
 
 nombre2 = ('Maria, Ana, Juana')
-
 palabras = nombre.split(' ')
 palabras_nombre = nombre2.split(',')
-
 print(palabras)
 print(palabras_nombre)
 
@@ -34,13 +24,12 @@ for palabra in palabras_nombre :
     print('palabra actual - ',palabra)
 
 print('---------Ejercicio Pregunta-----------')
-
-nombre3 = ('Ana Saes, Juana Saes, Jose Delgado')
+nombre3 = ('Ana Saes,Juana Saes,Jose Delgado')
 palabras_nombres3 = nombre3.split(',')
 
 for palabra in palabras_nombres3 :  
     print('Mujer completo -', palabra) 
     print('Mujer nombre simple -', palabra.split(' ')[0])
-    print('Mujer apellido simple - ', palabra.split(' ')[1])
+    print('Mujer apellido simple -', palabra.split(' ')[1])
 
 
