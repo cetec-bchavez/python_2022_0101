3050720e309a2d172a7c145ad80b738ec77042d4
EduClavijo68 <educlavijo68@gmail.com>
2022-02-05 12:12:55 -0500

    sd

---
+++ alumno4/06_COLECCIONES/02_tupla.py
@@ -0,0 +1,22 @@
+provincias=('Pichincha','Guayas','Carchi')
+print('Crear Provincias -> ',provincias)
+
+print('Primera Provincia -> ',provincias[0])
+
+#provincias[0]='PICHINCHA'
+#print('Cambio Primera Provincia -> ',provincias[0])
+
+#provincias.append('Azuay')
+#print('Nueva Provincia -> ',provincias)
+
+print('Numero Provincias',len(provincias))
+
+#provincias.remove('Guayas')
+#print('Quitar Provincia')
+print('Numero Provincias',len(provincias))
+print('Provincias Final -> ',provincias)
+
+print('Recorrer Provincias')
+for provincia in provincias :
+    print('Provincia =', provincia)
+    
