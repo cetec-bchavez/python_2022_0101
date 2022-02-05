c912688241a60cad7c6efbc5214cfdc7f2dcd949
ROMARDEL <richardomardelgado@hotmail.com>
2022-02-05 12:55:29 -0500

    :)

--- alumno1/07_clases/01_factura.py
+++ alumno1/07_clases/01_factura.py
@@ -13,6 +13,26 @@ class Factura:
         self.total=totalx
 
 
-factura1=Factura('Mouse',5.0,7.0,0.0)
+facturas= []
 
+factura1=Factura('Mouse',5.0,7.0,0.0)
+factura2=Factura('Teclado',5.0,7.0,0.0)
+
+factura1.producto='Mouse Genius'
+factura2.producto='Teclado Inx'
+
+
+facturas.append(factura1)
+facturas.append(factura2)
+
+print('------ Datos Factura 1 ------')
 print(factura1.producto,factura1.total)
+
+print('------ Datos Factura 2 ------')
+print(factura2.producto,factura2.total)
+
+print('------ Datos Facturas Todas ------')
+
+for facturax in facturas:   
+    print(facturax.producto, facturax.total)
+    
