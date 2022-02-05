26da04b452b6b7fb23f95fa87f75ea4cf6fecfec
EduClavijo68 <educlavijo68@gmail.com>
2022-02-05 00:02:01 -0500

    HJ

---
+++ instructor/00_indicaciones/tarea_1.py
@@ -0,0 +1,30 @@
+numero1 = 0.0
+numero2 = 0.0
+
+numero1 = float(numero1)
+numero2 = float(numero2)
+
+numero1 = input('INGRESE EL PRIMER VALOR = ')
+numero2 = input('INGRESE EL SEGUNDO VALOR = ')
+
+sumado = 0.0
+sumado = float(sumado)
+restado=0.0
+multiplicado=0.0
+dividido=0.0
+
+sumado = float(numero1) + float(numero2)
+
+print('Sumado=',sumado)
+
+restado = float(numero1) - float(numero2)
+
+print('Restado=',restado)
+
+multiplicado = float(numero1) * float(numero2)
+
+print('Multiplicado=',multiplicado)
+
+dividido = float(numero1) / float(numero2)
+
+print('Dividido=',dividido)
