b2a777ed8892e9ac24c35231a3fdcd7b81c9b150
ROMARDEL <richardomardelgado@hotmail.com>
2022-02-05 10:58:53 -0500

    :)

--- alumno1/05_funciones/03_funcion_lista_ingreso.py
+++ alumno1/05_funciones/03_funcion_lista_ingreso.py
@@ -20,9 +20,11 @@ promedio=0.0
 nota_actual= 0.0
 notas = [] #[5,4,7,9,10]
 
-nota_actual = input('ingrese nota: ') #ingresa dato, celda o variable, texto-string
+for x in range(3) :
 
-notas.append(float(nota_actual))
+    nota_actual = input('ingrese nota: ') #ingresa dato, celda o variable, texto-string
+
+    notas.append(float(nota_actual))
 
 
 promedio = calcular_promedio_lista(notas)
