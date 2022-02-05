80bde8889f2e749a1221edaa90efbdd40b57d582
ROMARDEL <richardomardelgado@hotmail.com>
2022-02-05 10:31:01 -0500

    solu?

--- alumno1/05_funciones/02_funcion_lista.py
+++ alumno1/05_funciones/02_funcion_lista.py
@@ -2,15 +2,23 @@ print('-------Funcion calculo con variables-------')
 
 #Definir Funcion
 def calcular_promedio_lista(valores):	#1) ingredientes
-	resultado = 0.0
-
-	suma=0.0
+    resultado = 0.0
+    suma= 0.0
     numero_valores= 0
-	#resultado = (valor1 + valor2) / 2 #2) Proceso
 
     numero_valores= len(valores)
 
     for valor in valores :
         suma+=valor
 
-	return resultado  #3) Resultado
+    resultado = suma / numero_valores
+
+    return resultado  #3) Resultado
+
+promedio=0.0
+notas = [5,4,7,9,10]
+
+promedio = calcular_promedio_lista(notas)
+
+print(promedio)
+print(notas)
