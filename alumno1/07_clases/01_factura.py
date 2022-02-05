e93d0efa3463d09321643fc6ef79b13a34a48dd2
ROMARDEL <richardomardelgado@hotmail.com>
2022-02-05 12:44:32 -0500

    :)

---
+++ alumno1/07_clases/01_factura.py
@@ -0,0 +1,18 @@
+class Factura:  
+    producto= ''
+    cantidad= 0
+    precio=0.0
+    total=0.0
+    
+
+    def __init__(self,productox,cantidadx,preciox,totalx):
+        
+        self.producto=productox
+        self.cantidad=cantidadx
+        self.precio=preciox
+        self.total=totalx
+
+
+factura1=Factura('Mouse',5.0,7.0,0.0)
+
+print(factura1.producto,factura1.total)
