e6e61cea6caec39bee26d6479178b1fb73543c52
cetec-bchavez <cetec.bchavez@gmail.com>
2022-02-05 13:00:44 -0500

    Factura Final

--- instructor/07_clases/factura.py
+++ instructor/07_clases/factura.py
@@ -5,6 +5,7 @@ class Factura:
     precio=0.0
     total=0.0
 
+    #Mecanico, PRIMEROS VALORES Ejm. Factura('Mouse',5.0,7.0,0.0)
     def __init__(self,productox,cantidadx,preciox,totalx):
 
         self.producto=productox
@@ -12,7 +13,32 @@ class Factura:
         self.precio=preciox
         self.total=totalx
 
+    def calcular_total(self):
+        self.total = self.cantidad * self.precio
 
-factura1 = Factura('Mouse',5.0,7.0,0.0)
 
+facturas=[] #Hoja Calculo completa (columnas, y filas de datos)
+factura1 = Factura('Mouse',5.0,7.0,0.0) # Fila factura1
+factura2 = Factura('Teclado',7.0,6.0,0.0) # Fila factura2
+
+factura1.producto='Mouse Genius' # Apuntamos a una Celda (producto) de fila factura1
+factura2.producto='Teclado Inx' # Apuntamos a una Celda (producto) de fila factura2
+
+factura1.calcular_total()
+factura2.calcular_total()
+
+facturas.append(factura1)
+facturas.append(factura2)
+
+print('------------- Datos Factura 1 --------------')
 print(factura1.producto,factura1.total)
+
+print('------------- Datos Factura 2 --------------')
+print(factura2.producto,factura2.total)
+
+
+print('------------- Datos Facturas TODOS --------------')
+
+for facturax in facturas: 
+    print(facturax.producto,facturax.total)
+    #print(facturax)
