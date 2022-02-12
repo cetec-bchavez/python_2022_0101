7eb9887f7b90604d78c36896b53c956d805c3641
cetec-bchavez <cetec.bchavez@gmail.com>
2022-02-12 12:58:40 -0500

    Guardar

---
+++ instructor/13_excepciones/General.py
@@ -0,0 +1,15 @@
+try: 
+    x=100
+    y=10
+    r= x/ y
+
+    print("Resultado",r)
+
+except:
+
+    print('Error Generado, consulte con Administrador')
+    #Codigo generar archivo text
+
+#finally:
+#    print('Siempre Ejecuta')
+    #Cerrar Archivos, conexiones a BD
